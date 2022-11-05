568c00e5617849e12303d90daf1ef656fede1ef9
SeNpAi <syzonenkod@gmail.com>
2022-11-05 17:21:02 +0100

    SmallHealthUpdate
    - Přidan skript EntityInventory, který se bude použivat pro dědění, sam skript dědí z NetworkBehaviour.
    - Přenos některých proměn z Player_Data do EntityInventory.
    - Změna dědíčnosti u Player_Data z NetworkBehaviour na EntityInventory.

---
+++ SurvivalGame/Assets/Scripts/AllEntities/EntityInventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class EntityInventory : NetworkBehaviour
+{
+    //Player Stats
+    public float health = 100f;
+    public float maxHealth = 100f;
+    public float stamina = 100f;
+    public float maxStamina = 100f;
+
+    //Chosen Item
+    public Transform currentObject;
+    public GameObject[] slots;
+    public GameObject chosenItemFrame;
+
+    public void CheckStats()
+    {
+        if (stamina > maxStamina)
+            stamina = maxStamina;
+        else if (stamina < 0)
+            stamina = 0;
+        if (health > maxHealth)
+            health = maxHealth;
+        //else if (health < 0)
+        //Die From cringe
+    }
+
+    //Možný pozdìjší health
+    /*public void TakeDamage(float value)
+    {
+        health -= value;
+    }*/
+}
--- SurvivalGame/Assets/Scripts/Player/Player_Data.cs
+++ SurvivalGame/Assets/Scripts/Player/Player_Data.cs
@@ -4,22 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
 
-public class Player_Data : NetworkBehaviour
+public class Player_Data : EntityInventory
 {
     public Slider healthBar;
+    public Slider staminaBar;
     [SerializeField]
     public NetworkVariable<float> networkPlayerHealth = new NetworkVariable<float>(100);
-    public float health;
-    public float maxHealth = 100f;
-    public Slider staminaBar;
-    public float stamina;
-    public float maxStamina = 100f;
     public float healthSendTime = 0f;
-
-    //Chosen Item
-    public Transform currentObject;
-    public GameObject[] slots;
-    public GameObject chosenItemFrame;
 
     private void Awake()
     {
@@ -34,8 +25,8 @@ public class Player_Data : NetworkBehaviour
     {
         healthSendTime--;
         healthBar.value = health;
-        staminaBar.value = stamina;  
-        Round();
+        staminaBar.value = stamina;
+        CheckStats();
         chosenItemFrame.transform.position = currentObject.transform.position;
 
         if (healthSendTime < 1f)
@@ -45,24 +36,6 @@ public class Player_Data : NetworkBehaviour
         }
 
     }
-
-    private void Round()
-    {
-        if (stamina > maxStamina)
-            stamina = maxStamina;
-        else if (stamina < 0)
-            stamina = 0;
-        if (health > maxHealth)
-            health = maxHealth;
-        //else if (health < 0)
-            //Die From cringe
-    }
-
-    //Možný pozdìjší health
-    /*public void TakeDamage(float value)
-    {
-        health -= value;
-    }*/
 
     [ServerRpc]
     private void SendClientHealthServerRpc()
